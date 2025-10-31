460c6ef87ef94ffd47268fb1b6830439493de097
Tamila <tamilasukaeva@gmail.com>
2025-10-31 23:39:55 +0500

    update for main

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
+import java.util.random.RandomGenerator;
+
 public class Main {
 
 // This is new branch
+        public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
 
-    public static void main(String[] args) {
-
-        System.out.println(createStr());
-
+            System.out.println("Hello number = "+ calc(i, RandomGenerator.getDefault().nextInt()));
+        }
     }
-
     static int calc(int a, int b) {
-
         if (a > b) {
             return a * b;
         } else if (b > 0) {
             return a / b;
+
         }
+        return 0;
+    }
+
+}
